0140dd05dd37dfcfcd163462fe582c9ae5ef4c92
sandeep-cloud-dba <35637721+sandeep-cloud-dba@users.noreply.github.com>
2026-08-13 06:13:55 -0500

    Add blocking check script using sp_whoisactive
    Added a script to check blocking and its chain using sp_whoisactive.

--- performance-tuning/8_Deadlock_and_Blocking.sql
+++ performance-tuning/8_Deadlock_and_Blocking.sql
@@ -1,3 +1,13 @@
+/*sp_whoisactive  -  script to check blocking and it's chain*/
+exec sp_whoisactive   @get_outer_command=1
+, @output_column_list = '[dd%][session_id][blocking_session_id][blocked_session_count][Wait_info][login_name][host_name][database_name][cpu][used_memory][sql_command][sql_text][wait_info]
+						[reads][writes][program_name][collection_time]'
+, @find_block_leaders=1,@sort_order = '[blocked_session_count] DESC'
+
+exec sp_whoisactive  @format_output=01,@get_plans=1, @get_additional_info=1 
+
+
+
 /*Deadlock In XML Format*/
 SELECT
     DATEADD
